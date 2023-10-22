48c2564f2520aa3d0fb392c21b7b782f6c3140cb
alexandraardelean1 <148080088+alexandraardelean1@users.noreply.github.com>
2023-10-22 17:38:03 +0300

    Add files via upload

---
+++ Pages/Authors/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ardelean_Alexandra_Lab2.Data;
+using Ardelean_Alexandra_Lab2.Models;
+
+namespace Ardelean_Alexandra_Lab2.Pages.Authors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ardelean_Alexandra_Lab2.Data.Ardelean_Alexandra_Lab2Context _context;
+
+        public IndexModel(Ardelean_Alexandra_Lab2.Data.Ardelean_Alexandra_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Author> Authors { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Authors != null)
+            {
+                Authors = await _context.Authors.ToListAsync();
+            }
+        }
+    }
+}
