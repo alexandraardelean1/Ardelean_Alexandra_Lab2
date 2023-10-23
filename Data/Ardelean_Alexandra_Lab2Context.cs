da2f628549d58b7043527d9215c0e07b8334f4f3
alexandraardelean1 <148080088+alexandraardelean1@users.noreply.github.com>
2023-10-23 19:31:14 +0300

    Add files via upload

---
+++ Data/Ardelean_Alexandra_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ardelean_Alexandra_Lab2.Models;
+
+namespace Ardelean_Alexandra_Lab2.Data
+{
+    public class Ardelean_Alexandra_Lab2Context : DbContext
+    {
+        public Ardelean_Alexandra_Lab2Context (DbContextOptions<Ardelean_Alexandra_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ardelean_Alexandra_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Ardelean_Alexandra_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Ardelean_Alexandra_Lab2.Models.Author>? Authors { get; set; }
+    }
+}
